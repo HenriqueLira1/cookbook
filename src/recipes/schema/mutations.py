9f5b7b47d589525330461d83e496743a35313b4e
Henrique Lira <hlira@mimic.com.br>
2020-12-01 09:43:58 -0300

    feat: Add delete mutations

--- src/recipes/schema/mutations.py
+++ src/recipes/schema/mutations.py
@@ -3,10 +3,10 @@ from django.shortcuts import get_object_or_404
 import graphene
 from graphene_django.rest_framework.mutation import SerializerMutation
 
-from recipes.models import Recipe
+from recipes.models import Ingredient, Recipe
 from recipes.serializers import IngredientSerializer, RecipeSerializer
 
-from .types import RecipeType
+from .types import IngredientType, RecipeType
 
 
 class CreateIngredientMutation(SerializerMutation):
@@ -19,6 +19,19 @@ class UpdateIngredientMutation(SerializerMutation):
     class Meta:
         serializer_class = IngredientSerializer
         model_operations = ["update"]
+
+
+class DeleteIngredientMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+
+    Output = IngredientType
+
+    def mutate(root, info, id):
+        instance = get_object_or_404(Ingredient, pk=id)
+        instance.delete()
+
+        return instance
 
 
 class RecipeInput(graphene.InputObjectType):
@@ -54,9 +67,24 @@ class UpdateRecipeMutation(graphene.Mutation):
         return serializer.save()
 
 
+class DeleteRecipeMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+
+    Output = RecipeType
+
+    def mutate(root, info, id):
+        instance = get_object_or_404(Recipe, pk=id)
+        instance.delete()
+
+        return instance
+
+
 class Mutation(graphene.ObjectType):
     create_ingredient = CreateIngredientMutation.Field()
     update_ingredient = UpdateIngredientMutation.Field()
+    delete_ingredient = DeleteIngredientMutation.Field()
 
     create_recipe = CreateRecipeMutation.Field()
     update_recipe = UpdateRecipeMutation.Field()
+    delete_recipe = DeleteRecipeMutation.Field()
